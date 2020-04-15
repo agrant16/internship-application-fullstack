e233e404e8a3bc054f1bb10ce0261a1b80091cbd
Alan Grant <agrant16@utk.edu>
2020-04-15 10:35:51 -0400

    Implement helper functions for random URL selection.

--- index.js
+++ index.js
@@ -20,6 +20,27 @@ const VARIANTS_URL = "https://cfw-takehome.developers.workers.dev/api/variants";
 const VARIANT_COOKIE_NAME = "variant_url";
 const REWRITER = new HTMLRewriter().on("*", new elementHandler());
 
+/**
+ * Returns a random index for the variant URLS array.
+ *
+ * Allows for approximately equal probabilty of choosing either
+ * URL 1 or URL 2. Especially as the number of requests grows large.
+ */
+function chooseRandomIndex() {
+  let x = random(2),
+    y = random(2);
+  if (x === y) return x;
+  else return chooseRandomIndex();
+}
+
+/**
+ * Helper function for chooseRandomIndex. Returns an int in [0, max).
+ * @param {int} max
+ */
+function random(max) {
+  return Math.floor(Math.random() * max);
+}
+
 addEventListener("fetch", event => {
   event.respondWith(handleRequest(event.request));
 });
